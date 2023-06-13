5f7b8e23d8f6695219a9a08d55c22dd82db62272
Preet <preetbhatia088@gmail.com>
2023-06-13 14:56:03 +0530

    Update day14.py

--- day14.py
+++ day14.py
@@ -1,24 +1,8 @@
-def build_pattern(n, m):
-    # Validate input conditions
-    if n % 2 == 0 or m != 3 * n:
-        print("Invalid input. The number 'n' must be odd and 'm' must be 3 times 'n'.")
-        return
-
-    # Top part of the pattern
-    for i in range(n // 2):
-        pattern = "-".join(["."] * ((m - 3 * (i * 2 + 1)) // 2))
-        print(f"{'-' * (3 * i)}{pattern}|{pattern[::-1]}{'-' * (3 * i)}")
-
-    # Middle part of the pattern
-    print(f"{'-' * ((m - 7) // 2)}WELCOME{'-' * ((m - 7) // 2)}")
-
-    # Bottom part of the pattern
-    for i in reversed(range(n // 2)):
-        pattern = "-".join(["."] * ((m - 3 * (i * 2 + 1)) // 2))
-        print(f"{'-' * (3 * i)}{pattern}|{pattern[::-1]}{'-' * (3 * i)}")
-
-
-# Test the function
-n = 7
-m = 21
-build_pattern(n, m)
+N, M = map(int, input().split())
+a = ".|."
+b = "WELCOME"
+for i in range(N//2):
+    print(((2*i + 1)*a).center(M,"-"))
+print((b).center(M,"-"))
+for i in reversed(range(N//2)):
+    print(((2*i + 1)*a).center(M,"-"))
