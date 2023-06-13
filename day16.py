7d6751c68c26b87dd2d10d76b290b67c876403c2
Preet <preetbhatia088@gmail.com>
2023-06-13 16:41:18 +0530

    Create day16.py

---
+++ day16.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    capitalized_string=s.title()
+    return capitalized_string
+    
+   
+
+if __name__ == '__main__':
+    
+
+    s = input()
+
+    result = solve(s)
+    print (result)
+
+
+
