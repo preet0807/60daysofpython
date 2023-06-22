a214cb512c6ff50943eae68379bff95d541c4f11
Preet <preetbhatia088@gmail.com>
2023-06-22 14:13:08 +0530

    day 19

---
+++ array 1_1.py
@@ -0,0 +1,24 @@
+#max and min in an array 
+
+# n= int(input("enter the size of the array"))
+# lst=[]
+# for i in range (1,n):
+    
+#     a=int(input())
+#     lst.append(a)
+# max_lst=max(lst)
+# min_lst=min(lst)
+# print("maximum number in array is:",max_lst)
+# print("minimum number in array is:" ,min_lst)
+ 
+
+#REverse the array
+
+lst=[1,2,3,4,5,6]
+
+
+
+# lst.reverse()
+# print(lst)
+
+print(lst[::-1])
