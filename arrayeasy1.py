a547d6db388ce1a43c2dd2d0e3e944d35a927a5a
Preet <preetbhatia088@gmail.com>
2023-06-22 14:13:15 +0530

    day 20

---
+++ arrayeasy1.py
@@ -0,0 +1,14 @@
+n=int(input("enter the size of array"))
+arr=[]
+for i in range(0,n):
+    c=int(input())
+    arr.append(c)
+    lst=[]
+for i in range(0 ,1):
+    d=max(arr)
+    arr.remove(d)
+e=max(arr)
+print(e)
+
+    
+
