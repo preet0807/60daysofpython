1b91800481a7200e0b87b0850b4f97bad80a6251
Preet <preetbhatia088@gmail.com>
2023-06-25 15:39:13 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -6,11 +6,46 @@
 #  2. STRING last
 #
 
-def print_full_name(first_name, last_name):
-    res= print(f"Hello {first_name} {last_name}! You just delved into python")
-    return res
+# def print_full_name(first_name, last_name):
+#     res= print(f"Hello {first_name} {last_name}! You just delved into python")
+#     return res
 
-if __name__ == '__main__':
-    first_name = input()
-    last_name = input()
-    print_full_name(first_name, last_name)
+# if __name__ == '__main__':
+#     first_name = input()
+#     last_name = input()
+#     print_full_name(first_name, last_name)
+
+# Create a String
+# myStr = 'counterdemonstration'
+
+# # Display the String
+# print("String = ", myStr)
+
+# #Slice the string
+# # Negative Indexing with step
+# print("String after slicing (negative indexing) = ", myStr[-7:-2])
+
+# nums=[3,-1,4,-2,0]
+# [x for x in nums if x>(sum(nums)/len(nums))]
+
+# for i,v in enumerate (['Python', 'Java','C++']):
+#     print(i,v)
+
+class Student:
+    def _init_(self,name,address,phone):
+
+        self.name=name
+
+        self.address-address
+
+        self.phone-phone
+
+    name="john"
+
+    std=Student(name,F786543')
+
+    name="marcus"
+
+    std.phone='975150
+
+    print(std.name,std.phone)
